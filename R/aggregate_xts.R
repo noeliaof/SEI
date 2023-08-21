3927a9b905e30b1c2dab908e98929b703464354a
sallen12 <sam.allen@stat.unibe.ch>
2023-08-21 14:42:52 +0200

    add documentation to aggregate_xts function

--- R/aggregate_xts.R
+++ R/aggregate_xts.R
@@ -7,7 +7,7 @@
 #' @param len length of the aggregation period.
 #' @param scale timescale of the aggregation period, default is 'days'.
 #' @param fun function to apply to the aggregated data, default is 'sum'.
-#' @param timescale timescale of \code{x}.
+#' @param timescale timescale of \code{x}, default is 'days'.
 #' @param na_thres threshold for the percentage of NA values allowed in the
 #'  aggregation period, default = 10.
 #'
@@ -15,13 +15,48 @@
 #' This has been adapted from code available at
 #' \url{https://github.com/WillemMaetens/standaRdized}.
 #'
+#' \code{len} is a single numeric value specifying over how many time units the
+#' data \code{x} is to be aggregated. By default, \code{len} is assumed to correspond
+#' to a number of days, but this can also be specified manually using the argument
+#' \code{scale}. \code{scale} must be one of: "days", "weeks", "months", "quarters", and "years".
+#'
+#' \code{fun} determines the function used to aggregate the time series. By default,
+#' \code{fun = "sum"}, meaning the aggregation results in accumulations over the
+#' aggregation period. Alternative functions can also be used. For example, specifying
+#' \code{fun = "mean"} would return the mean over the aggregation period.
+#'
+#' \code{timescale} is the timescale of the input data \code{x}. By default, this
+#' is assumed to be "days".
+#'
+#' Since the time series \code{x} aggregates data over the aggregation period, problems
+#' may arise when \code{x} contains missing values. For example, if interest is
+#' on daily accumulations, but 50% of the values in the aggregation period are missing,
+#' the accumulation over this aggregation period will not be accurate.
+#' This can be controlled using the argument \code{na_thres}.
+#' \code{na_thres} specifies the percentage of NA values in the aggregation period
+#' before a NA value is returned. i.e. the proportion of values that are allowed to be missing.
+#' The default is \code{na_thres = 10}.
+#'
 #' @return
 #' An xts time series with aggregated values.
 #'
 #' @author Sam Allen, Noelia Otero
 #'
 #' @examples
-#' Examples of the aggregate_xts function will be added here
+#' data(data_supply)
+#' # consider hourly German energy supply data in 2019
+#' supply_de <- subset(data_supply, country == "Germany", select = c("date", "PWS"))
+#' supply_de <- xts::xts(supply_de$PWS, order.by = supply_de$date)
+#' options(xts_check_TZ = FALSE)
+#'
+#' # daily accumulations
+#' supply_de_daily <- aggregate_xts(supply_de, len = 1, timescale = "hours")
+#'
+#' # weekly means
+#' supply_de_weekly <- aggregate_xts(supply_de, len = 1, scale = "weeks", fun = "mean", timescale = "hours")
+#'
+#' plot(supply_de)
+#' plot(supply_de_weekly)
 #'
 #' @name aggregate_xts
 NULL
@@ -37,9 +72,9 @@ aggregate_xts <- function(x,
   scale <- match.arg(scale)
   timescale <- match.arg(timescale)
   x_agg <- sapply(index(x), aggregate_xts_1, x, len, scale, fun, timescale, na_thres)
-  x_agg <- xts(x_agg, order.by = index(x))
-  xtsAttributes(x_agg) <- xtsAttributes(x)
-  xtsAttributes(x_agg)$agg_length <- as.difftime(len, units = scale)
+  x_agg <- xts::xts(x_agg, order.by = index(x))
+  xts::xtsAttributes(x_agg) <- xts::xtsAttributes(x)
+  xts::xtsAttributes(x_agg)$agg_length <- as.difftime(len, units = scale)
   return(x_agg)
 }
 
@@ -55,7 +90,7 @@ aggregate_xts_1 <- function(date,
   to <- date
   data <- data[paste(from, to, sep = '/')]
   dates <- seq(from = from, to = to, by = as.difftime(1, units = timescale))
-  x <- c(coredata(merge(xts(order.by = dates), data, join = 'left')))
+  x <- c(coredata(merge(xts::xts(order.by = dates), data, join = 'left')))
 
   if (length(x) != 0) {
     pct.na <- (length(which(is.na(x))) / length(x)) * 100
