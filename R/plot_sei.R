e7b1eb5f56b71997cf80eba9ed2aa055d059ac55
sallen12 <sam.allen@stat.unibe.ch>
2023-08-24 11:47:58 +0200

    add ggplot dependencies to plot_sei

--- R/plot_sei.R
+++ R/plot_sei.R
@@ -7,6 +7,7 @@
 #' @param type type of plot (either time series "ts", or histogram "hist").
 #' @param title optional title of the plot.
 #' @param lab axis label (y-axis if type = "ts", x-axis if type = "hist").
+#' @param ylims lower and upper limits of the time series plot.
 #' @param n_bins the number of bins to show in the histogram.
 #'
 #' @return
@@ -39,30 +40,30 @@ plot_sei <- function(x, type = c("ts", "hist"), title = NULL, lab = "Std. Index"
 
   if (type == "ts") {
     df <- zoo::fortify.zoo(x)
-    p <- ggplot(df, aes(x = Index, y = x)) +
-      geom_ribbon(aes(ymin = pmin(x, 0), ymax = 0), fill = "blue", alpha = 0.5) +
-      geom_ribbon(aes(ymin = 0, ymax = pmax(x, 0)), fill = "red", alpha = 0.5) +
-      scale_x_datetime(name = "Date", expand = c(0, 0)) +
-      scale_y_continuous(name = lab, limits = ylims, expand = c(0, 0)) +
-      geom_hline(aes(yintercept = 0), color = "grey", linetype = "dotted") +
-      theme_bw() +
-      ggtitle(title)
+    p <- ggplot2::ggplot(df, ggplot2::aes(x = Index, y = x)) +
+      ggplot2::geom_ribbon(ggplot2::aes(ymin = pmin(x, 0), ymax = 0), fill = "blue", alpha = 0.5) +
+      ggplot2::geom_ribbon(ggplot2::aes(ymin = 0, ymax = pmax(x, 0)), fill = "red", alpha = 0.5) +
+      ggplot2::scale_x_datetime(name = "Date", expand = c(0, 0)) +
+      ggplot2::scale_y_continuous(name = lab, limits = ylims, expand = c(0, 0)) +
+      ggplot2::geom_hline(ggplot2::aes(yintercept = 0), color = "grey", linetype = "dotted") +
+      ggplot2::theme_bw() +
+      ggplot2::ggtitle(title)
   } else if (type == "hist") {
     if (xts::is.xts(x)) {
       df <- zoo::fortify.zoo(x)
     } else {
       df <- data.frame(x = x)
     }
-    p <- ggplot(df) +
-      geom_histogram(aes_string(x = as.numeric(x)), col = "black", bins = n_bins, alpha = 0.4) +
-      xlab(lab) +
-      scale_y_continuous(name = "Density", expand = expansion(c(0, 0.05))) +
-      theme_bw() +
-      theme(panel.grid.major = element_blank(),
-            panel.grid.minor = element_blank(),
-            axis.text.y = element_blank(),
-            axis.ticks.y = element_blank()) +
-      ggtitle(title)
+    p <- ggplot2::ggplot(df) +
+      ggplot2::geom_histogram(ggplot2::aes_string(x = as.numeric(x)), col = "black", bins = n_bins, alpha = 0.4) +
+      ggplot2::xlab(lab) +
+      ggplot2::scale_y_continuous(name = "Density", expand = ggplot2::expansion(c(0, 0.05))) +
+      ggplot2::theme_bw() +
+      ggplot2::theme(panel.grid.major = ggplot2::element_blank(),
+                     panel.grid.minor = ggplot2::element_blank(),
+                     axis.text.y = ggplot2::element_blank(),
+                     axis.ticks.y = ggplot2::element_blank()) +
+      ggplot2::ggtitle(title)
   }
 
   return(p)
